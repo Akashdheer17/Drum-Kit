81ea89eddac6db505ba910c492ac2f66045c10af
Akash <161685064+Akashdheer17@users.noreply.github.com>
2025-04-09 18:28:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+// Function to play sound based on button key
+function playSound(key) {
+  const button = document.querySelector("." + key);
+
+  if (button) {
+    button.style.color = "white";
+    setTimeout(() => {
+      button.style.color = "";
+    }, 200);
+
+    const bgImage = window.getComputedStyle(button).backgroundImage;
+    console.log("Full BG Image:", bgImage);
+
+    const imageName = bgImage.split('/').pop().replace(/["')]/g, "");
+    console.log("Extracted image name:", imageName);
+
+    const audioName = imageName.replace(".png", ".mp3");
+    console.log("Final audio file name:", audioName);
+
+    const audio = new Audio("./sounds/" + audioName);
+    audio.play().catch(err => console.log("Audio play error:", err));
+  }
+}
+
+// Mouse click event
 document.querySelectorAll(".drum").forEach(button => {
-    button.addEventListener("click", () => {
-      button.style.color = "white";
-      setTimeout(() => {
-        button.style.color = "";
-      }, 200);
-  
-      const bgImage = window.getComputedStyle(button).backgroundImage;
-      console.log("Full BG Image:", bgImage);
-  
-      const imageName = bgImage.split('/').pop().replace(/["')]/g, "");
-      console.log("Extracted image name:", imageName);
-  
-      const audioName = imageName.replace(".png", ".mp3");
-      console.log("Final audio file name:", audioName);
-  
-      const audio = new Audio("./sounds/" + audioName);
-      audio.play().catch(err => console.log("Audio play error:", err));
-    });
+  button.addEventListener("click", () => {
+    const key = button.innerHTML.toLowerCase();
+    playSound(key);
   });
-  
+});
+
+// Keyboard press event
+document.addEventListener("keydown", (event) => {
+  const key = event.key.toLowerCase();
+  playSound(key);
+});
